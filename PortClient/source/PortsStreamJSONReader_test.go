7c0c65a9187340504f97db7bba1d297b197a92a9
Eugene M <gaddeena@gmail.com>
2020-08-11 18:19:00 +0300

    Added a test for successful parsing.

--- PortClient/source/PortsStreamJSONReader_test.go
+++ PortClient/source/PortsStreamJSONReader_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/x1n13y84issmd42/portobello/PortClient/source"
+	"github.com/x1n13y84issmd42/portobello/shared/models"
 )
 
 func TestPortsStreamJSONReader(T *testing.T) {
@@ -53,6 +54,95 @@ func TestPortsStreamJSONReader(T *testing.T) {
 
 		expected := source.JSONInvalidPortID("666")
 		actual := <-errorChan
+
+		assert.Equal(T, expected, actual)
+	})
+
+	T.Run("Alright", func(T *testing.T) {
+		r := strings.NewReader(`{
+			"AEAJM": {
+			  "name": "Ajman",
+			  "city": "Ajman",
+			  "country": "United Arab Emirates",
+			  "alias": [],
+			  "regions": [],
+			  "coordinates": [
+				55.5136433,
+				25.4052165
+			  ],
+			  "province": "Ajman",
+			  "timezone": "Asia/Dubai",
+			  "unlocs": [
+				"AEAJM"
+			  ],
+			  "code": "52000"
+			},
+			"AEAUH": {
+			  "name": "Abu Dhabi",
+			  "coordinates": [
+				54.37,
+				24.47
+			  ],
+			  "city": "Abu Dhabi",
+			  "province": "Abu Z¸aby [Abu Dhabi]",
+			  "country": "United Arab Emirates",
+			  "alias": [],
+			  "regions": [],
+			  "timezone": "Asia/Dubai",
+			  "unlocs": [
+				"AEAUH"
+			  ],
+			  "code": "52001"
+			}
+		}`)
+
+		errorChan := make(source.ErrorChannel)
+		portsChan := source.PortsStreamJSONReader(r, errorChan)
+
+		expected := []*models.Port{
+			{
+				ID:      "AEAJM",
+				Name:    "Ajman",
+				City:    "Ajman",
+				Country: "United Arab Emirates",
+				Alias:   []string{},
+				Regions: []string{},
+				Coordinates: []float64{
+					55.5136433,
+					25.4052165,
+				},
+				Province: "Ajman",
+				Timezone: "Asia/Dubai",
+				Unlocs: []string{
+					"AEAJM",
+				},
+				Code: "52000",
+			},
+			{
+				ID:      "AEAUH",
+				Name:    "Abu Dhabi",
+				City:    "Abu Dhabi",
+				Country: "United Arab Emirates",
+				Alias:   []string{},
+				Regions: []string{},
+				Coordinates: []float64{
+					54.37,
+					24.47,
+				},
+				Province: "Abu Z¸aby [Abu Dhabi]",
+				Timezone: "Asia/Dubai",
+				Unlocs: []string{
+					"AEAUH",
+				},
+				Code: "52001",
+			},
+		}
+
+		var actual []*models.Port
+
+		for p := range portsChan {
+			actual = append(actual, p)
+		}
 
 		assert.Equal(T, expected, actual)
 	})
